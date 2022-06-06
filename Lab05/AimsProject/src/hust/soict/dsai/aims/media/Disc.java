f443a678ba58eb31aa903c780fe364b8ab7564a3
Charles <tuan.lda204929@sis.hust.edu.vn>
2022-06-06 22:34:35 +0700

    add demo lab05

---
+++ Lab05/AimsProject/src/hust/soict/dsai/aims/media/Disc.java
@@ -0,0 +1,41 @@
+package hust.soict.dsai.aims.media;
+
+public abstract class Disc extends Media implements Playable {
+	private String director;
+	private int length;
+	
+	public Disc(String title, String category, String director, float cost) {
+		super(title, category, cost);
+		this.director = director;
+	}
+	public Disc(String title, String category, String director, int length, float cost) {
+		super(title, category, cost);
+		this.director = director;
+		this.length = length;
+	}
+
+	public Disc(String title, String category, float cost) {
+		super(title, category, cost);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Disc(String title, String category) {
+		super(title, category);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Disc(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+}
+   
---
+++ Lab05/AimsProject/src/hust/soict/dsai/aims/media/MediaComparatorByTitleCost.java
@@ -0,0 +1,21 @@
+package hust.soict.dsai.aims.media;
+import java.util.Comparator;
+
+public class MediaComparatorByTitleCost implements Comparator<Media> {
+
+	public int compare(Media m1, Media m2) {
+		int titleComparison = m1.getTitle().compareToIgnoreCase(m2.getTitle());
+		if (titleComparison != 0) {
+			return titleComparison;
+		} else {
+			if (m1.getCost() < m2.getCost()) {
+				return 1;
+			} else if (m1.getCost() > m2.getCost()) {
+				return -1;
+			} else {
+				return 0;
+			}
+		}
+	}
+}
+   
